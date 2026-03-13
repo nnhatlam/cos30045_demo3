8976f69254378be84dc26e6353f4471f86325e57
nhatlam <sbsbnnl@gmail.com>
2026-03-13 14:52:29 +0700

    redo ex 4.5

--- d3.js
+++ d3.js
@@ -1,9 +1,10 @@
 const svg = d3.select('.responsive-svg-container')
     .append('svg')
-        .attr("width", "100%")
-        .attr("height", 600)
-        .attr("viewBox", "0 0 500 600")
+        .attr("viewBox", "0 0 500 1600")
         .style("border", "1px solid black")
+
+svg
+    .append('rect')
 
 d3.csv("data/brand_model.csv").then(data => {console.log(data)});
 d3.csv("data/brand_model.csv", d => {
@@ -20,32 +21,17 @@ d3.csv("data/brand_model.csv", d => {
     createBarChart(data);
 });
 const createBarChart = (data) => {
-    const chartTopY = 0;
-    const chartBaseY = 1310;
-    const chartLeftX = 0;
-    const chartRightX = 500;
-    const maxModels = d3.max(data, d => d.no_models) || 1;
 
-    const xScale = d3.scaleLinear()
-        .domain([0, maxModels])
-        .range([chartLeftX, chartRightX]);
-
-    const yScale = d3.scaleBand()
-        .domain(data.map((d) => d.brand))
-        .range([chartTopY, chartBaseY])
-        .padding(0.2);
-
-    svg
-        .selectAll('.bar')
-        .data(data)
-        .join("rect")
-        .attr("x", chartLeftX)
-        .attr("y", (d, i) => yScale(i))
-        .attr("width", d => xScale(d.no_models) - chartLeftX)
-        .attr("height", yScale.bandwidth())
-        .attr("fill", "#1c71df")
-        .attr("class", data =>{
-            console.log(data);
-            return `bar bar-${data.no_models}`;
-        })
+    svg.selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("x", 0)
+    .attr("class", data => {
+        console.log(data);
+        return `bar bar-${data.no_models}`;
+    })
+    .attr("y", (data, i) => i * 30)
+    .attr("width", data => data.no_models * 10)
+    .attr("height", 20)
+    .attr("fill", "steelblue");
 }
